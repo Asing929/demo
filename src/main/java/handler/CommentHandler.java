d51c3f7034bb59dfc4ab57fe8d47476168fd44b3
Asing929 <3085345049@qq.com>
2024-11-27 09:16:21 +0800

    update

---
+++ src/main/java/handler/CommentHandler.java
@@ -0,0 +1,49 @@
+package handler;
+
+import model.Article;
+import model.Comment;
+import model.User;
+import repository.ArticleRepository;
+import repository.CommentRepository;
+import repository.UserRepository;
+import security.Secured;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/comments")
+public class CommentHandler {
+
+    @Inject
+    private CommentRepository commentRepository;
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Inject
+    private ArticleRepository articleRepository;
+
+    @POST
+    @Path("/")
+    @Secured({"user", "admin"})
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createComment(Comment comment , @Context SecurityContext securityContext ) {
+        User user = userRepository.findByID(Integer.valueOf(securityContext.getUserPrincipal().getName()));
+        comment.setUser(user);
+        Article article = articleRepository.findByID(comment.getArticleId());
+        comment.setArticle(article);
+        comment.setCreatedAt(System.currentTimeMillis() / 1000);
+        commentRepository.create(comment);
+        Map<String, Object> res = new HashMap<>();
+        res.put("code", Response.Status.OK);
+        return Response.status(Response.Status.OK).entity(res).build();
+
+
+        }
+    }
---
+++ src/main/java/handler/IndexHandler.java
@@ -0,0 +1,13 @@
+package handler;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/")
+public class IndexHandler {
+
+    @GET
+    public String index() {
+        return "Hello World!";
+    }
+}
